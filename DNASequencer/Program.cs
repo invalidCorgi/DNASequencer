cdff2175a3e25446ae29ecf4a3d3a09b394b285f
invalidcorgi <invalidcorgi@gmail.com>
2019-03-22 18:54:26 +0100

    Add project files.

---
+++ DNASequencer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DNASequencer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
