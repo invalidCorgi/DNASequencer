510946b6a0e8b790000dc7bfd3d5964190fa19cc
Mikołaj Pudlicki <mpudlicki@man.poznan.pl>
2019-03-27 12:41:52 +0100

    Added functions which creates empty arrows between verices

--- DNASequencer/Graph.cs
+++ DNASequencer/Graph.cs
@@ -13,6 +13,24 @@ namespace DNASequencer
 
         public void MakeEmptyArrowsBetweenVertices()
         {
+            for (int i = 0; i < Count - 1; i++)
+            {
+                for (int j = i + 1; j < Count; j++)
+                {
+                    for (int k = 0; k < this[i].Sequence.Length; k++)
+                    {
+                        if (this[i].Sequence.EndsWith(this[j].Sequence.Substring(k)))
+                        {
+                            Arrow arrow = new Arrow();
+                            arrow.From = this[i];
+                            arrow.To = this[j];
+                            arrow.Distance = k;
+                            this[j].Predecessors.Add(arrow);
+                            this[i].Successors.Add(arrow);
+                        }
+                    }
+                }
+            }
             throw new NotImplementedException();
         }
 
